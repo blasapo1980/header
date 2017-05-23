99c5e8c040066ca15464ed0b9c902a0fb4d6b469
Blasapo <blasapo@gmail.com>
2017-05-23 13:53:28 +0200

    Cierre menu desplegable

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 var boton = document.querySelector(".btn-menu");
-var menu = document.querySelector(".show-menu");
+
 
 function toggle() {
- menu.style.display="block";
+  var menu = document.querySelector(".show-menu");
+  if (menu.style.display==="none" || menu.style.display===""){
+    menu.style.display="block";
+  } else {
+    menu.style.display="none";
+  }
 }
-
 boton.addEventListener('click', toggle);
