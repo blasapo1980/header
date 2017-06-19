205a30df089b697bd79be9991be6c5270d860255
Blasapo <blasapo@gmail.com>
2017-06-19 11:08:48 +0200

    add menu with js

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 var boton = document.querySelector(".btn-menu");
-var menu = document.querySelector(".menu-mobile");
 
 function toggle() {
-  if (menu.classList.contains('hidden')) {
-    menu.classList.remove('hidden');
-
-  } else {
-    menu.classList.add('hidden');
-  }
+  var menu = document.querySelector(".menu-mobile");
+  menu.classList.toggle('hidden');
 }
+
 boton.addEventListener('click', toggle);
+
+var menuFullScreen = document.querySelector(".menu-full-screen");
+var menuItems = [
+  "About",
+  "blog",
+  "contact"
+];
+
+
+for (var i = 0; i < menuItems.length; i++) {
+  menuFullScreen.innerHTML += '<li><a href="#">'+menuItems[i]+'</a></li>';
+}
